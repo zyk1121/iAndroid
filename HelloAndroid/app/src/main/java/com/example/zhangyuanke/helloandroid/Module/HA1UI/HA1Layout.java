31641b5fa12928317621646b9f6c6cce5084627c
张元科 <zhangyuanke@leador.com.cn>
2017-05-22 23:38:04 +0800

    XML布局

--- HelloAndroid/app/src/main/java/com/example/zhangyuanke/helloandroid/Module/HA1UI/HA1Layout.java
+++ HelloAndroid/app/src/main/java/com/example/zhangyuanke/helloandroid/Module/HA1UI/HA1Layout.java
@@ -7,6 +7,8 @@ import com.example.zhangyuanke.helloandroid.Tools.BaseActivity;
 
 public class HA1Layout extends BaseActivity {
 
+    // android:layout_gravity="" 用于指定控件在布局中的对齐方式：当线性布局是vertical时，水平方向的设置生效
+    // android:gravity=""用于指定文字在控件中的对齐方式
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
